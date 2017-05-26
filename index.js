dadca73b04d947865fdb72097ad194d00480833b
Travis Jefferson <traviscjefferson@gmail.com>
2017-05-26 11:52:22 -0500

    Add a POST create for quotes

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const ENV = process.env['NODE_ENV'] || 'development';
 const PORT = Number(process.env['PORT'] || 8080);
 const DB_CONFIG = require('./knexfile');
 const knex = require('knex')(DB_CONFIG);
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
 
 app.set('view engine', 'pug');
 
 app.get('/', function(req, res) {
-  res.send('Shit tony says!');
+  res.redirect(301, '/quotes');
 });
 
 app.get('/quotes', function(req, res) {
@@ -22,8 +29,9 @@ app.get('/quotes/new', function(req, res) {
 });
 
 app.post('/quotes', function(req, res) {
-  console.log('req', req.headers);
-  res.send('naaaah');
+  knex('quotes').insert({ body: req.body.quote }).then(() => {
+    res.render('quotes/new');
+  }).catch(console.error.bind(console));
 });
 
 app.listen(PORT, function() {
