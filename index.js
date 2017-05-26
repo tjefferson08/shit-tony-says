fed8b1949dc69a0314ae4a266d01a9cc28152e54
Travis Jefferson <traviscjefferson@gmail.com>
2017-05-25 23:31:57 -0500

    Add views and first DB query what!?

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const app = express();
-const knex = require('knex');
 const ENV = process.env['NODE_ENV'] || 'development';
 const PORT = Number(process.env['PORT'] || 8080);
 const DB_CONFIG = require('./knexfile');
+const knex = require('knex')(DB_CONFIG);
+
+app.set('view engine', 'pug');
 
 app.get('/', function(req, res) {
-  res.send('Shit tony says is coming soon! SO SOON');
+  res.send('Shit tony says!');
+});
+
+app.get('/quotes', function(req, res) {
+  knex.select().table('quotes').then((quotes) => {
+    res.render('quotes/index', { quotes });
+  });
+});
+
+app.get('/quotes/new', function(req, res) {
+  res.render('quotes/new');
+});
+
+app.post('/quotes', function(req, res) {
+  console.log('req', req.headers);
+  res.send('naaaah');
 });
 
 app.listen(PORT, function() {
